ff83e6b707f784920ac73dc367cefde800331f87
Jorch <jorch.rl@gmail.com>
2021-09-27 09:16:17 -0500

    State Example

--- part-1/statefulcomponent/src/App.jsx
+++ part-1/statefulcomponent/src/App.jsx
@@ -2,11 +2,27 @@ import { useState } from "react";
 
 const App = () => {
     const [counter, setCounter] = useState(0);
+    const [reset, setReset] = useState(false);
 
-    setTimeout(() => setCounter(counter + 1), 1000);
+    //setTimeout(() => setCounter(counter + 1), 1000);
+
+    const handleClick = () => {
+        setCounter(counter + 1);
+        if (!reset) setReset(true);
+    };
+
+    const handleReset = () => {
+        if (reset) {
+            setCounter(0);
+            setReset(false);
+        }
+    };
+
     return (
-        <div className="App">
-            <div>{counter}</div>
+        <div className='App'>
+            <h1>Counter: {counter}</h1>
+            <button onClick={handleClick}>Click me!</button>
+            {reset && <button onClick={handleReset}>Reset me!</button>}
         </div>
     );
 };
