2603fb0c8c7fdb1817727d7f25df244c35e4fc1f
Jorch <jorch.rl@gmail.com>
2021-09-30 00:09:05 -0500

    Finish stateful component examples from part 1.c

--- part-1/statefulcomponent/src/AppStateful.jsx
+++ part-1/statefulcomponent/src/AppStateful.jsx
@@ -1,5 +1,19 @@
 import React, { useState } from "react";
 
+// Helper components
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return <div>The app is used by pressing the buttons</div>;
+    }
+
+    return <div>Button press history: {props.allClicks.join(" - ")}</div>;
+};
+
+const Button = ({ clickHandler, text }) => {
+    return <button onClick={clickHandler}>{text}</button>;
+};
+
+// Main component
 const AppStateful = () => {
     const [left, setLeft] = useState(0);
     const [right, setRight] = useState(0);
@@ -17,10 +31,11 @@ const AppStateful = () => {
     return (
         <div>
             {left}
-            <button onClick={handleLeft}>left</button>
-            <button onClick={handleRight}>right</button>
+            <Button clickHandler={handleLeft} text={"left"} />
+            <Button clickHandler={handleRight} text={"Right"} />
             {right}
-            <p>{history.join(" - ")}</p>
+            {/* <p>Num. of clicks= {history.length}</p> */}
+            <History allClicks={history} />
         </div>
     );
 };
