2a8fca7221390733fd85b94a9d22579fc13b1de4
Jorch <jorch.rl@gmail.com>
2021-09-30 00:59:46 -0500

    Finish excercise 1.7

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -1,11 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
+    const [stats, setStats] = useState({});
 
     const getHandler = (val, setVal) => () => setVal(val + 1);
+
+    useEffect(() => {
+        const all = good + bad + neutral;
+        const score = good - bad;
+        const average = all !== 0 ? score / all : 0;
+
+        const positive = all !== 0 ? (good / all) * 100 : 0;
+
+        setStats({
+            all: all,
+            average: average,
+            positive: positive,
+        });
+    });
 
     return (
         <>
@@ -18,6 +33,9 @@ const App = () => {
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>all {stats.all}</p>
+            <p>average {stats.average}</p>
+            <p>positive {stats.positive}%</p>
         </>
     );
 };
