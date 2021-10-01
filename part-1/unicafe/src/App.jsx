f4d58a9f37e274fc819e21e5746a7e8c18d3210f
Jorch <jorch.rl@gmail.com>
2021-10-01 16:40:58 -0500

    Finish excercise 1.8

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -1,26 +1,35 @@
 import React, { useState, useEffect } from "react";
 
+const Statistics = ({ reviews }) => {
+    const { good, neutral, bad } = reviews;
+
+    // We no longer need a stats state as the component will re-render
+    // when the props are updated by its parent!
+    const all = good + bad + neutral;
+    const score = good - bad;
+    const average = all !== 0 ? score / all : 0;
+
+    const positive = all !== 0 ? (good / all) * 100 : 0;
+
+    return (
+        <>
+            <h2>Statistics</h2>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+            <p>all {all}</p>
+            <p>average {average}</p>
+            <p>positive {positive}%</p>
+        </>
+    );
+};
+
 const App = () => {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
-    const [stats, setStats] = useState({});
 
     const getHandler = (val, setVal) => () => setVal(val + 1);
-
-    useEffect(() => {
-        const all = good + bad + neutral;
-        const score = good - bad;
-        const average = all !== 0 ? score / all : 0;
-
-        const positive = all !== 0 ? (good / all) * 100 : 0;
-
-        setStats({
-            all: all,
-            average: average,
-            positive: positive,
-        });
-    });
 
     return (
         <>
@@ -29,13 +38,7 @@ const App = () => {
             <button onClick={getHandler(neutral, setNeutral)}>neutral</button>
             <button onClick={getHandler(bad, setBad)}>bad</button>
             <br />
-            <h2>Statistics</h2>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {stats.all}</p>
-            <p>average {stats.average}</p>
-            <p>positive {stats.positive}%</p>
+            <Statistics reviews={{ good: good, neutral: neutral, bad: bad }} />
         </>
     );
 };
