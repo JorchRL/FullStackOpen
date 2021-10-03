9cfffa3fc80bf2d9ec50b93c9febb0d95c479dc1
Jorch <jorch.rl@gmail.com>
2021-10-03 05:09:54 -0500

    Init Notes example

--- part-2/Notes/src/App.jsx
+++ part-2/Notes/src/App.jsx
@@ -1,9 +1,16 @@
-function App() {
+import React from "react";
+
+const App = ({ notes }) => {
     return (
-        <div className="App">
-            <h1>Hello, world!</h1>
+        <div>
+            <h1>Notes</h1>
+            <ul>
+                <li>{notes[0].content}</li>
+                <li>{notes[1].content}</li>
+                <li>{notes[2].content}</li>
+            </ul>
         </div>
     );
-}
+};
 
 export default App;
--- part-2/Notes/src/index.jsx
+++ part-2/Notes/src/index.jsx
@@ -2,4 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        date: "",
+        important: true,
+    },
+    {
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        date: "",
+        important: false,
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "",
+        important: true,
+    },
+];
+
+ReactDOM.render(<App notes={notes} />, document.getElementById("root"));
