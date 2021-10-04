1cd78ea2c4c5b75e7fc1b26e54ff9878c79c5a0c
Jorch <jorch.rl@gmail.com>
2021-10-03 21:09:50 -0500

    Finish Notes examples for part 2.b

--- part-2/Notes/src/App.jsx
+++ part-2/Notes/src/App.jsx
@@ -1,15 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Note from "./components/Note";
 
-const App = ({ notes }) => {
+const App = (props) => {
+    const [notes, setNotes] = useState(props.notes);
+    const [newNote, setNewNote] = useState("");
+    const [showAll, setShowAll] = useState(false);
+
+    const addNote = (event) => {
+        event.preventDefault();
+        // console.log("button clicked", event.target);
+        setNotes(
+            notes.concat({
+                id: notes.length + 1,
+                content: newNote,
+                date: new Date().toISOString(),
+                important: Math.random() < 0.5,
+            })
+        );
+        setNewNote("");
+        // console.log("Click event target", event.target[0]);
+        // console.log("value: ", event.target[0].value);
+
+        // Reset the value in the text input area
+        event.target[0].value = "";
+        // console.log("value: ", event.target[0].value);
+    };
+
+    const handleNoteChange = (event) => {
+        // console.log(event.target.value);
+        setNewNote(event.target.value);
+    };
+
+    const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
     return (
         <div>
             <h1>Notes</h1>
+            <div>
+                <button onClick={() => setShowAll(!showAll)}>show {showAll ? "important" : "all"}</button>
+            </div>
             <ul>
-                {notes.map((note) => (
+                {notesToShow.map((note) => (
                     <Note key={note.id} note={note} />
                 ))}
             </ul>
+            <hr />
+            <form onSubmit={addNote}>
+                <input onChange={handleNoteChange} placeholder={"Add a new note here..."} />
+                <button type='submit' disabled={newNote.length <= 0}>
+                    Add Note
+                </button>
+            </form>
         </div>
     );
 };
