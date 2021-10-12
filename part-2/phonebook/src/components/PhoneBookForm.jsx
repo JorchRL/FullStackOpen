f4e9cb542cecc568695bc73d32e1db5ff74d1eef
Jorch <jorch.rl@gmail.com>
2021-10-12 11:04:13 -0500

    Finish excercise 2.10; Remove unused variables in PhoneBookForm.jsx component

--- part-2/phonebook/src/components/PhoneBookForm.jsx
+++ part-2/phonebook/src/components/PhoneBookForm.jsx
@@ -1,36 +1,13 @@
-import React, { useState } from "react";
-import Debug from "./Debug";
-
 const PhoneBookForm = ({ addNameHandler }) => {
-    const [newName, setNewName] = useState("");
-    const [newNumber, setNewNumber] = useState("");
-
-    const handleInputChange = (event) => {
-        // console.log(event.target);
-        event.target.id === "name-input" && setNewName(event.target.value);
-        event.target.id === "number-input" && setNewNumber(event.target.value);
-    };
-
-    const debugVars = [
-        {
-            name: "newName",
-            val: newName,
-        },
-        {
-            name: "newNumber",
-            val: newNumber,
-        },
-    ];
-
     return (
         <div>
             {/* <Debug vars={debugVars} /> */}
             <h2>Add new</h2>
             <form onSubmit={addNameHandler}>
                 <div>
-                    name: <input id='name-input' onChange={handleInputChange} type='text' placeholder='Add a new name...' />
+                    name: <input id='name-input' type='text' placeholder='Add a new name...' />
                     <br />
-                    number: <input id='number-input' onChange={handleInputChange} type='text' placeholder='Add a new number...' />
+                    number: <input id='number-input' type='text' placeholder='Add a new number...' />
                 </div>
                 <div>
                     <button type='submit'>add</button>
