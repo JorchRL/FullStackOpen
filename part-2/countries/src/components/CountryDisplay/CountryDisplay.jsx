8a4cf9eaa9b3cc8ddae97053597ce99d841077aa
Jorch <jorch.rl@gmail.com>
2021-10-14 12:52:58 -0500

    Fix a bug when rendering CountryCard before weather data is received

--- part-2/countries/src/components/CountryDisplay/CountryDisplay.jsx
+++ part-2/countries/src/components/CountryDisplay/CountryDisplay.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import "./CountryDisplay.css";
 
 const CountryDisplay = ({ name, flag, capital, population, languages }) => {
-    const [capitalWeather, setCapitalWeather] = useState(0);
+    const [capitalWeather, setCapitalWeather] = useState([]);
 
     // console.log(process.env);
 
@@ -22,34 +22,38 @@ const CountryDisplay = ({ name, flag, capital, population, languages }) => {
 
     return (
         <div className='Dcontainer'>
-            <div className='Dinfo'>
-                <div className='info'>
-                    <h3 id='Dname'>{name}</h3>
-                    <p id='Dcapital'>Capital city: {capital}</p>
-                    <p id='Dpopulation'>Population: {population}</p>
+            {capitalWeather.temperature !== undefined && (
+                <>
+                    <div className='Dinfo'>
+                        <div className='info'>
+                            <h3 id='Dname'>{name}</h3>
+                            <p id='Dcapital'>Capital city: {capital}</p>
+                            <p id='Dpopulation'>Population: {population}</p>
 
-                    <h4>Languages</h4>
-                    <ul>
-                        {languages.map((lang) => {
-                            return <li key={lang.name}>{lang.name}</li>;
-                        })}
-                    </ul>
-                </div>
-                <div className='weather'>
-                    <h3>{`Weather in ${capital}`}</h3>
-                    <img src={capitalWeather.weather_icons[0]} alt='' />
-                    <p>
-                        Tempearture: <br /> {capitalWeather.temperature} celsius{" "}
-                    </p>
-                    <p>
-                        Wind: <br /> {capitalWeather.wind_speed} km/hr, dir: {capitalWeather.wind_dir}{" "}
-                    </p>
-                </div>
-            </div>
-            <h3 className='info'>{`${name}'s flag`}</h3>
-            <div id='Dflag'>
-                <img src={flag} alt={`Flag of ${name}`} />
-            </div>
+                            <h4>Languages</h4>
+                            <ul>
+                                {languages.map((lang) => {
+                                    return <li key={lang.name}>{lang.name}</li>;
+                                })}
+                            </ul>
+                        </div>
+                        <div className='weather'>
+                            <h3>{`Weather in ${capital}`}</h3>
+                            <img src={capitalWeather.weather_icons[0]} alt='' />
+                            <p>
+                                Tempearture: <br /> {capitalWeather.temperature} celsius{" "}
+                            </p>
+                            <p>
+                                Wind: <br /> {capitalWeather.wind_speed} km/hr, dir: {capitalWeather.wind_dir}{" "}
+                            </p>
+                        </div>
+                    </div>
+                    <h3 className='info'>{`${name}'s flag`}</h3>
+                    <div id='Dflag'>
+                        <img src={flag} alt={`Flag of ${name}`} />
+                    </div>{" "}
+                </>
+            )}
         </div>
     );
 };
--- part-2/countries/src/index.jsx
+++ part-2/countries/src/index.jsx
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App, { CountryDisplayDEBUG } from "./App";
 
-ReactDOM.render(<CountryDisplayDEBUG />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
