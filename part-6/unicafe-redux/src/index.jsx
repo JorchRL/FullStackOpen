571d16fa098a4291a3c116b4d40d374ddb8e9d16
Jorch <jorch.rl@gmail.com>
2021-11-22 14:53:43 -0600

    Exercise 6.2

--- part-6/unicafe-redux/src/index.jsx
+++ part-6/unicafe-redux/src/index.jsx
@@ -3,24 +3,28 @@ import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import counterReducer from "./reducer";
 
-const store = createStore(counterReducer);
+const store = createStore(
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const App = () => {
-  const goodHandler = () => {
+  const countHandler = (_type) => {
+    console.log(`dispatch of type ${_type}`);
     store.dispatch({
-      type: "GOOD",
+      type: _type,
     });
   };
 
   return (
     <div>
-      <button onClick={goodHandler}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={() => countHandler("GOOD")}>good</button>
+      <button onClick={() => countHandler("OK")}>ok</button>
+      <button onClick={() => countHandler("BAD")}>bad</button>
+      <button onClick={() => countHandler("ZERO")}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   );
 };
