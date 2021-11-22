bf4a27b605c5f2a586ea3dd30811466a895e1a94
Jorch <jorch.rl@gmail.com>
2021-11-22 14:01:08 -0600

    noteReducer example from part 6.a (before exercises)

--- part-6/counter-redux/src/index.jsx
+++ part-6/counter-redux/src/index.jsx
@@ -1,5 +1,44 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import React from "react";
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(noteReducer);
+
+store.dispatch({
+  type: "NEW_NOTE",
+  data: {
+    content: "the app state is in redux store",
+    important: true,
+    id: 1,
+  },
+});
+
+store.dispatch({
+  type: "NEW_NOTE",
+  data: {
+    content: "state changes are made with actions",
+    important: true,
+    id: 1,
+  },
+});
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        {store.getState().map((note) => (
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? "important" : ""}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+};
+
+renderApp();
+store.subscribe(renderApp);
